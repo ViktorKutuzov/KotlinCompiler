9dfef0b7eee9e0d03a8d9113db503008ab493203
IconMatthew <jadaevmatvei@gmail.com>
2024-10-01 21:48:01 +0300

    Добавление тестов для чисел с плавающей точкой

--- lexer/start.bat
+++ lexer/start.bat
@@ -3,3 +3,5 @@ flex lexer.l
 gcc lex.yy.c -o a.exe
 
 type test\input\basic_operators_i.txt | a.exe > test\output\basic_operators_o.txt
+type test\input\double_numbers_i.txt | a.exe > test\output\double_numbers_o.txt
+type test\input\e_double_numbers_i.txt | a.exe > test\output\e_double_numbers_o.txt
