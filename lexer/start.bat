dce42fa716105e8adc308490723be1742d1cc7be
Viktor Kutuzov <kutuzoviktor03@gmail.com>
2024-10-01 21:58:51 +0300

    Добавил тесты

--- lexer/start.bat
+++ lexer/start.bat
@@ -3,3 +3,4 @@ flex lexer.l
 gcc lex.yy.c -o a.exe
 
 type test\input\basic_operators_i.txt | a.exe > test\output\basic_operators_o.txt
+type test\input\floating_points_numbers_i.txt | a.exe > test\output\floating_points_numbers_o.txt
