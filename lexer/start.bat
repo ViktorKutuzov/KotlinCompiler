57a841f7725f2d76601b23bca23cc0ab1613476d
IconMatthew <jadaevmatvei@gmail.com>
2024-10-01 22:51:41 +0300

    Обновление файла запуска

--- lexer/start.bat
+++ lexer/start.bat
@@ -6,3 +6,4 @@ type test\input\basic_operators_i.txt | a.exe > test\output\basic_operators_o.tx
 type test\input\double_numbers_i.txt | a.exe > test\output\double_numbers_o.txt
 type test\input\e_double_numbers_i.txt | a.exe > test\output\e_double_numbers_o.txt
 type test\input\floating_points_numbers_i.txt | a.exe > test\output\floating_points_numbers_o.txt
+type test\input\string_literal_i.txt | a.exe > test\output\string_literal_o.txt
